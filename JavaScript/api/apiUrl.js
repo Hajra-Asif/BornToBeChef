617b4363ff5786175780af05c0ddf918cd718240
Hajra-Asif <hajramasif@gmail.com>
2025-02-17 15:02:36 +0500

    first commit

---
+++ JavaScript/api/apiUrl.js
@@ -0,0 +1,21 @@
+//ramzan api url :::::::        https://www.themealdb.com/api/json/v1/1/search.php?s
+     // ramzan api url detail page:::  https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idd}
+
+// lunch dinner api url :::::     https://www.themealdb.com/api/json/v1/1/categories.php
+   // lunch api url detail page:::  https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idd}
+
+
+
+
+// drinks api url :::::     https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail
+  // drinks api url detail page:::    https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idd}
+
+
+
+// desserts api url :::::   https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert
+ // desserts api url detail page:::   https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idd}
+
+
+
+// breakfast api url ::::   https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast
+ // breakfast api url detail page:::   https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}
